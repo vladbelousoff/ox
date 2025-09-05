72606e7aae0c583c5f75994e1d25c5ab7f353386
Vladislav Belousov <vladbelousov@yahoo.com>
2025-09-05 22:18:56 +0100

    More structures in ox_ecs.h

--- code/ox_ecs.h
+++ code/ox_ecs.h
@@ -40,12 +40,56 @@ typedef struct {
 } ox_component_info_t;
 
 typedef struct {
+  bitset_t* bitset;
+} ox_component_mask_t;
+
+static void ox_component_mask_init(ox_component_mask_t* self)
+{
+  self->bitset = bitset_create_with_capacity(OX_COMPONENTS_MAX);
+}
+
+static void ox_component_mask_term(const ox_component_mask_t* self)
+{
+  bitset_free(self->bitset);
+}
+
+typedef struct {
   ox_component_info_t components[OX_COMPONENTS_MAX];
   size_t component_count;
-  bitset_t* all_components_mask;
+  ox_component_mask_t all_components_mask;
 } ox_component_registry_t;
 
 void ox_component_registry_init(ox_component_registry_t* registry);
+
+typedef struct {
+  void* data;
+  size_t capacity;
+  size_t used;
+} ox_memory_chunk_t;
+
+typedef struct {
+  ox_memory_chunk_t chunks[OX_ECS_POOL_MAX_CHUNKS];
+  size_t chunk_count;
+  size_t element_size;
+  size_t elements_per_chunk;
+} ox_memory_pool_t;
+
+typedef struct {
+  ox_component_mask_t component_mask;
+  ox_component_mask_t all_components_mask;
+
+  // Component pools (one per component type)
+  ox_memory_pool_t* component_pools;
+  size_t component_pool_count;
+
+  size_t entity_count;
+  size_t capacity;
+} ox_archetype_t;
+
+typedef struct {
+  ox_component_mask_t include_mask;
+  ox_component_mask_t exclude_mask;
+} ox_query_filter_t;
 
 typedef struct {
   ox_component_registry_t component_registry;

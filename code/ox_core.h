f04c276e2f77f5bd04960d6f83eb996da5b87cae
Vladislav Belousov <vladbelousov@yahoo.com>
2025-09-05 22:09:28 +0100

    Renames i n ox_ecs.h

--- code/ox_core.h
+++ code/ox_core.h
@@ -6,3 +6,8 @@
 
 #define OX_ARRAY_SIZE(x)                                                       \
   _Generic(&(x), __typeof__(&(x)): (sizeof(x) / sizeof((x)[0])))
+
+#define OX_DECLARE_ID(name)                                                    \
+  typedef struct {                                                             \
+    int value;                                                                 \
+  } name
--- code/ox_ecs.h
+++ code/ox_ecs.h
@@ -1,33 +1,26 @@
 #pragma once
 
-#include "bitset.h"
+#include "ox_core.h"
 
+#include <bitset.h>
 #include <stdint.h>
 
 // Configuration constants
-#define OX_COMPONENTS_MAX    448
-#define OX_ARCHETYPES_MAX    1024
-#define OX_QUERIES_MAX       1024
-#define OX_ENTITY_NONCE_BITS 24
-#define OX_ENTITY_INDEX_BITS 24
+#define OX_COMPONENTS_MAX              448
+#define OX_ECS_ARCHETYPES_MAX          1024
+#define OX_ECS_QUERIES_MAX             1024
+#define OX_ENTITY_NONCE_BITS           24
+#define OX_ENTITY_INDEX_BITS           24
+#define OX_ECS_POOL_DEFAULT_CHUNK_SIZE 512
+#define OX_ECS_POOL_MAX_CHUNKS         128
+
 #define OX_ENTITY_USER_DATA_BITS                                               \
   (sizeof(uint64_t) * 8 - OX_ENTITY_NONCE_BITS - OX_ENTITY_INDEX_BITS)
 
-typedef struct {
-  int value;
-} ox_component_id_t;
-
-typedef struct {
-  int value;
-} ox_query_id_t;
-
-typedef struct {
-  int value;
-} ox_system_id_t;
-
-typedef struct {
-  int value;
-} ox_archetype_id_t;
+OX_DECLARE_ID(ox_component_id);
+OX_DECLARE_ID(ox_query_id);
+OX_DECLARE_ID(ox_system_id);
+OX_DECLARE_ID(ox_archetype_id);
 
 // Entity structure (64-bit handle)
 typedef struct {
@@ -39,7 +32,7 @@ typedef struct {
     };
     uint64_t value;
   };
-} ox_entity_t;
+} ox_entity_id;
 
 typedef struct {
   size_t size;
@@ -56,7 +49,7 @@ void ox_component_registry_init(ox_component_registry_t* registry);
 
 typedef struct {
   ox_component_registry_t component_registry;
-  ox_entity_t* entities;
+  ox_entity_id* entities;
   size_t entities_count;
   size_t entities_capacity;
 } ox_world_t;
